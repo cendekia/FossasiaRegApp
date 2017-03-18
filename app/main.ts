ef824ccc1f72abd2af2919b7dc89f9ed7ab72718
Cendekia Pramana Putra <me@cendekiapp.com>
2017-03-19 00:16:51 +0800

    implementing firebase on main app

--- app/main.ts
+++ app/main.ts
@@ -3,4 +3,18 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
+import firebase = require("nativescript-plugin-firebase");
+
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+}).then(
+  (instance) => {
+    console.log("firebase.init done");
+  },
+  (error) => {
+    console.log("firebase.init error: " + error);
+  }
+);
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
