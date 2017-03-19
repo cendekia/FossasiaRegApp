aa0331a79d747625d47ff848adf9a67cf80ad6b7
Cendekia Pramana Putra <me@cendekiapp.com>
2017-03-19 08:56:02 +0800

    trying to communicate with firebase

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from "@angular/core";
 
+import firebase = require("nativescript-plugin-firebase");
+
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
 })
-export class AppComponent { }
+export class AppComponent {
+
+	constructor() {
+		firebase.login({
+			type: firebase.LoginType.ANONYMOUS
+		}).then((user) => {
+			alert("user id : " + user.uid);
+		}, (error) => {
+			alert("error " + error);
+		})
+	}
